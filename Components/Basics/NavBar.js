1d2fc3b5a5bb1b9d9d16becfdecb7e97827c60e2
vishal jais <vishal.jais00@gmail.com>
2023-12-18 23:24:12 +0530

    persist done

--- Components/Basics/NavBar.js
+++ Components/Basics/NavBar.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useSelector } from 'react-redux';
+import { userData } from '@/store/features/User/userReducer';
+
 
 
 function NavBar() {
-    // const userLoadData = useSelector((state)=>state.user)
+    const userLoadData = useSelector(userData)
+    console.log(userLoadData,"userLoadData navbar")
   return (
     
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -13,6 +17,7 @@ function NavBar() {
               My Dash board
             </Link>
             <div className="collapse navbar-collapse">
+            
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
                   <Link href="/dashboard">
