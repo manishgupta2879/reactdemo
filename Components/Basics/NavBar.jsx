028c8395cb694fa9c6e9b2f17571b2e5c691215f
vishal jais <vishal.jais00@gmail.com>
2023-12-19 10:48:50 +0530

    loader implemented

--- Components/Basics/NavBar.jsx
+++ Components/Basics/NavBar.jsx
@@ -14,7 +14,6 @@ const Navbar = () => {
   };
 
   const userFetchData = useSelector(userData)
-  console.log("userFetchData",userFetchData)
   //------------------------------Navbar------------------------------------------------------------
 
   return (
